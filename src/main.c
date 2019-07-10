abab202fccb1c05da9bed83c50126e987c8bb30d
t4th <tath@o2.pl>
2019-07-10 10:25:03 +0200

    Cleanup.

--- src/main.c
+++ src/main.c
@@ -41,23 +41,6 @@ typedef struct kernel_s
 
 volatile kernel_t kernel;
 
-volatile __INLINE __asm void store_context(volatile context_t * p)
-{
-    stm r0, {r4-r11};
-    bx lr;
-}
-
-volatile __INLINE __asm void load_context(volatile context_t * p)
-{
-    ldm r0, {r4-r11};
-    bx lr;
-}
-
-__ASM void handler(void)
-{
-  MRS r0,PSP
-  ;movs r1, =kernel
-}
 
 volatile uint32_t * current_task_context = 0;
 volatile uint32_t * next_task_context = 0;
@@ -90,47 +73,6 @@ void SysTick_Handler(void)
     }
 }
 
-extern Contex_Switch(void);
-
-void PendSV_Handler2(void)
-{    
-    kernel.thread[kernel.current_thread].sp = __get_PSP(); // store current sp
-    store_context(&kernel.thread[kernel.current_thread].context);
-
-    load_context(&kernel.thread[kernel.next_thread].context);
-    __set_PSP( kernel.thread[kernel.next_thread].sp); // set next sp
-
-    kernel.current_thread = kernel.next_thread;
-
-    kernel.next_thread ++;
-
-    if (kernel.next_thread  >= NO_THREADS)
-    {
-        kernel.next_thread = 0;
-    }
-}
-
-volatile uint32_t * c_context1 = (volatile uint32_t *) &kernel.thread[0].context;
-volatile uint32_t * c_context2 = (volatile uint32_t *) &kernel.thread[1].context;
-
-void c_switch(void) 
-{
-    kernel.thread[kernel.current_thread].sp = __get_PSP(); // store current sp
-    c_context1 = (volatile uint32_t *)&kernel.thread[kernel.current_thread].context;
-
-    c_context2 = (volatile uint32_t *)&kernel.thread[kernel.next_thread].context;
-    __set_PSP( kernel.thread[kernel.next_thread].sp); // set next sp
-    
-    kernel.current_thread = kernel.next_thread;
-    
-    kernel.next_thread ++;
-    if (kernel.next_thread  >= NO_THREADS)
-    {
-        kernel.next_thread = 0;
-    }
-}
-
-#if 1
 __ASM void PendSV_Handler(void)
 {
     extern current_task_context;
@@ -146,43 +88,10 @@ __ASM void PendSV_Handler(void)
     ldr r1, [r0]
     ldm r1, {r4-r11};
     
-    ldr r0, =0xFFFFFFFD
+    ldr r0, =0xFFFFFFFD ; return to thread mode. Without this PendSV would return to SysTick losing current thread state along the way.
     CPSIE I
     bx r0
     nop
-}
-#else
-__ASM void PendSV_Handler(void)
-{
-    extern c_context1;
-    extern c_context2;
-    extern c_switch;
-    
-    ldr r14, =next;
-    B c_switch;
-    
-next
-    ldr r0, =c_context1;
-    ldr r1, [r0]
-    stm r1, {r4-r11};
-    
-    ldr r0, =c_context2;
-    ldr r1, [r0]
-    ldm r1, {r4-r11};
-    
-    ldr r0, =0xFFFFFFFD
-    bx r0
-    nop
-}
-#endif
-
-void thread_proc(int thread_id)
-{
-    if (thread_id < NO_THREADS)
-    {
-        kernel.thread[thread_id].proc(); // run
-        // clean up after thread
-    }
 }
 
 void led(GPIO_PIN_t pin)
@@ -269,23 +178,14 @@ void init_kernel(void)
     kernel.thread[0].proc(); // run
 }
 
-__ASM void start_kernel(void)
-{
-    // set sp
-    ;mov R1, #2;
-    ;MRS CONTROL, R1;
-    ;mov R1, #0;
-    ;MRS r14, R1;
-}
-
 int main()
 {    
     RCC->APB2ENR |= RCC_APB2ENR_IOPFEN; // enable gpiof
     
-//    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN6, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
-//    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN7, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
-//    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN8, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
-//    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN9, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
+    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN6, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
+    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN7, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
+    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN8, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
+    Gpio_SetPinAsOutput(GPIOF, GPIO_PIN9, GPIO_SPEED_50MHZ, GPIO_GP_PUSH_PULL);
  
     SysTick_Config(72000-1); // 1ms
     init_kernel();
