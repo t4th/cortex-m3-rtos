98053240755a219d38f1159a82aad39a4b407578
tath <tath@o2.pl>
2020-03-31 20:24:21 +0200

    Add inline assembly and kernel interface.

--- source/kernel/hardware/armv7m/hardware.cpp
+++ source/kernel/hardware/armv7m/hardware.cpp
@@ -1,4 +1,5 @@
 #include <hardware.hpp>
+#include <kernel.hpp>
 
 #include <stm32f10x.h>
 
@@ -27,12 +28,23 @@ namespace kernel::hardware
     }
 }
 
+// TODO: Make this somehow private to kernel::hardware or nameless namespace.
+// This is workaround to use C++ variables symbols in assembly language.
+volatile uint32_t * current_task_context = 0;
+volatile uint32_t * next_task_context = 0;
+volatile uint32_t skip_store = 0;
+
 extern "C"
 {
     void SysTick_Handler(void)
     {
-        int a;
-        a++;
+        __disable_irq();
+        
+        kernel::tick();
+        
+        __DSB(); // This is not needed in this case, due to write buffer being cleared on interrupt exit,
+                 // but it is nice to have explicit information that memory write delay is taken into account.
+        __enable_irq();
     }
 }
 
@@ -40,8 +52,31 @@ extern "C"
 {
     void PendSV_Handler(void)
     {
-    __ASM ("CPSID I");
+        __ASM
+        (
+        "CPSID I;"
+
+        "store_current_task: "
+        "ldr r0, =current_task_context; "
+        "ldr r1, [r0];"
+        "stm r1, {r4-r11};"
         
-    __ASM ("CPSIE I");
+        "load_next_task:"
+        "ldr r0, =next_task_context;"
+        "ldr r1, [r0];"
+        "ldm r1, {r4-r11};"
+        
+        // Set skip_store to 0.
+        "mov r0, #0;"
+        "nop;"
+        "str r0, [r2];"
+        
+        // 0xFFFFFFFD in r0 means 'return to thread mode'.
+        // Without this PendSV would return to SysTick
+        // losing current thread state along the way.
+        "ldr r0, =0xFFFFFFFD ;"
+        "CPSIE I; "
+        "bx r0"
+        );
     }
 }
--- source/kernel/kernel.cpp
+++ source/kernel/kernel.cpp
@@ -14,5 +14,6 @@ namespace kernel
     
     void start()
     {
+        hardware::start();
     }
 }
