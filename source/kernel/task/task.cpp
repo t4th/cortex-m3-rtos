ce98dad00f72a04e0821fb73d1313af15b7441d6
tath <tath@o2.pl>
2020-04-02 19:20:36 +0200

    Initial kernal::task.

--- source/kernel/task/task.cpp
+++ source/kernel/task/task.cpp
@@ -1,5 +1,35 @@
 #include <task.hpp>
 
+#include <memory_buffer.hpp>
+
+namespace
+{
+    struct task_info
+    {
+        uint32_t a;
+        uint32_t b;
+        uint32_t c;
+    };
+    
+    struct
+    {
+        kernel::common::MemoryBuffer<task_info, 64> m_data;
+    } m_context;
+}
+
 namespace kernel::task
 {
+    bool create(
+        Routine     a_routube,
+        Priority    a_priority,
+        uint32_t *  a_handle,
+        bool        a_create_suspended
+        )
+    {
+        uint32_t item_id;
+        
+        //m_context.m_data.allocate(item_id);
+        
+        return false;
+    }
 }
--- source/kernel/task/task.hpp
+++ source/kernel/task/task.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <cstdint>
+
 namespace kernel::task
 {
+    typedef void(*Routine)(void);  // TODO: Add argument.
+    
+    enum class Priority
+    {
+        High,
+        Medium,
+        Low
+    };
+    
+    bool create(
+        Routine     a_routube,
+        Priority    a_priority = kernel::task::Priority::Low,
+        uint32_t *  a_handle = nullptr,
+        bool        a_create_suspended = false
+        );
 }
