628a21375f0d5f4c1ceac3ea1dc142960a01ac4c
tath <tath@o2.pl>
2020-04-02 17:24:03 +0200

    Initial memory buffer implementation.

--- source/kernel/common/memory_buffer.cpp
+++ source/kernel/common/memory_buffer.cpp
@@ -1,5 +1,39 @@
 #include <memory_buffer.hpp>
 
-namespace kernel::memory_buffer
+namespace kernel::common
 {
+    template <typename TDataType, std::size_t MaxSize>
+    bool MemoryBuffer<TDataType, MaxSize>::allocate(uint32_t & a_item_id)
+    {
+        // Find first not used slot and return index as ID.
+        for (uint32_t i = 0; i < MaxSize; ++i)
+        {
+            if (false == m_status[i])
+            {
+                m_status[i] = true;
+                a_item_id = i;
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    template <typename TDataType, std::size_t MaxSize>
+    void MemoryBuffer<TDataType, MaxSize>::free(uint32_t & a_item_id)
+    {
+        if (a_item_id < MaxSize)
+        {
+            m_status[a_item_id] = false;
+        }
+    }
+    
+    template <typename TDataType, std::size_t MaxSize>
+    TDataType & MemoryBuffer<TDataType, MaxSize>::get(uint32_t & a_item_id)
+    {
+        // TODO: Make it safe. For now, implementation assumes input data is
+        // always valid since this is used only by kernel modules.
+        
+        return m_data[a_item_id];
+    }
 }
--- source/kernel/common/memory_buffer.hpp
+++ source/kernel/common/memory_buffer.hpp
@@ -1,5 +1,20 @@
 #pragma once
 
-namespace kernel::event
+#include <array>
+
+namespace kernel::common
 {
+    // Abstract memory buffer. Idea behind it is to keep data and status separetly as
+    // array of structs data structure.
+    template <typename TDataType, std::size_t MaxSize>
+    class MemoryBuffer
+    {
+        private:
+            std::array<TDataType, MaxSize>  m_data;
+            std::array<bool, MaxSize>       m_status; // TODO: Is bool 1 byte or 4? Hack it to 1-bit - 1-status.
+        public:
+            bool allocate(uint32_t & a_item_id);
+            void free(uint32_t & a_item_id);
+            TDataType & get(uint32_t & a_item_id);
+    };
 }
