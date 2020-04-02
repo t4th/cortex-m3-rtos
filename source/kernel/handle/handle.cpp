15ea6fcd98eccc2ede5d6f6c8b18df0ac23308cb
tath <tath@o2.pl>
2020-04-02 18:52:00 +0200

    Initial kernel::handle.

--- source/kernel/handle/handle.cpp
+++ source/kernel/handle/handle.cpp
@@ -1,5 +1,32 @@
 #include <handle.hpp>
 
+#include <memory_buffer.hpp>
+
 namespace kernel::handle
 {
+    constexpr size_t max_number_of_handles = 64;
+    
+    uint32_t create(uint32_t index, ObjectType type)
+    {
+        uint32_t new_handle = (static_cast<uint32_t>(type) << 16) | (index & 0xFFFF);
+        
+        return new_handle;
+    }
+    
+    void close(uint32_t & handle)
+    {
+        
+    }
+    
+    uint32_t getIndex(uint32_t handle)
+    {
+        return handle & 0xFFFF;
+    }
+    
+    ObjectType getObjectType(uint32_t handle)
+    {
+        uint32_t object_type = (handle >> 16) & 0xFFFF;
+        
+        return static_cast<ObjectType>(object_type);
+    }
 }
--- source/kernel/handle/handle.hpp
+++ source/kernel/handle/handle.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
+#include <cstdint>
+
 namespace kernel::handle
 {
+    enum class ObjectType : uint32_t
+    {
+        task = 0,
+        timer,
+        event
+    };
+    
+    uint32_t create(uint32_t index, ObjectType type);
+    void close(uint32_t & handle);
+    
+    uint32_t getIndex(uint32_t handle);
+    ObjectType getObjectType(uint32_t handle);
 }
