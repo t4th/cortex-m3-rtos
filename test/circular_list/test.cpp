58a606f1fdf4cc88f407c38c467326cd3b19a8ee
tath <tath@o2.pl>
2020-04-18 15:55:12 +0200

    Add include target file dir.

--- test/circular_list/test.cpp
+++ test/circular_list/test.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include "circular_list.hpp"
+
 TEST_CASE("test template")
 {
     REQUIRE(1 == 1);
