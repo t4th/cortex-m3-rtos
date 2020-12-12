f8fbe6d75c15c7f3bff1c62b72acee4572efe57c
tath <tath@o2.pl>
2020-12-12 19:43:11 +0100

    Add some user API descriptions.

--- source/kernel/kernel.hpp
+++ source/kernel/kernel.hpp
@@ -14,10 +14,11 @@ namespace kernel
     typedef volatile void * Handle;
 
     // Initialize kernel.
+    // This is PRE-CONDITION to all other kernel functions!
+    // If not called first, kernel behaviour is UNDEFINED.
     void init();
 
-    // Start kernel.
-    // Pre-condition: run kernel::init() before.
+    // Start kernel. If no user tasks were created, jumps to IDLE task.
     void start();
 
     // Get time elapsed since kernel started in miliseconds.
@@ -29,6 +30,8 @@ namespace kernel::task
 {
     typedef void(*Routine)(void * a_parameter);
 
+    // Task priority. Idle task MUST always be available and MUST
+    // always be last in Priority enum. Otherwise UB.
     enum class Priority
     {
         High = 0U,
@@ -45,7 +48,8 @@ namespace kernel::task
         Running
     };
 
-    // Create new task.
+    // Create new task. Can be create statically (before calling kernel::start()),
+    // and in run-time, by other tasks.
     bool create(
         kernel::task::Routine   a_routine,
         kernel::task::Priority  a_priority = kernel::task::Priority::Low,
@@ -57,16 +61,20 @@ namespace kernel::task
     // Get Handle to currently running task.
     kernel::Handle getCurrent();
 
-    // Brute force terminate task.
+    // Brute force terminate task. Can cause UB if task is system object owner, or
+    // task was inside critical section.
     void terminate( kernel::Handle & a_handle);
 
+    // Suspend task execution. Task can suspend itself.
     void suspend( kernel::Handle & a_handle);
 
+    // Works only with Suspended tasks. Has no effect on Ready or Waiting tasks.
     void resume( kernel::Handle & a_handle);
 
     void sleep( Time_ms a_time);
 }
 
+// User API for controling software timers.
 namespace kernel::timer
 {
     bool create(
@@ -78,16 +86,21 @@ namespace kernel::timer
     void stop( kernel::Handle & a_handle);
 }
 
+// User API for controling events.
 namespace kernel::event
 {
+    // If a_manual_reset is set to false, event will be reset when waitForObject
+    // function completes. In other cause, you have to manualy call reset.
     bool create( kernel::Handle & a_handle, bool a_manual_reset = false);
     void destroy( kernel::Handle & a_handle);
     void set( kernel::Handle & a_handle);
     void reset( kernel::Handle & a_handle);
 }
 
+// User API for controling critical section.
 namespace kernel::critical_section
 {
+    // Modyfing this outsice critical_section API is UB.
     struct Context
     {
         volatile std::atomic<uint32_t> m_lockCount;
@@ -96,9 +109,13 @@ namespace kernel::critical_section
         kernel::Handle m_ownerTask; // debug information
     };
 
+    // Spinlock argument define number of ticks used to check critical section
+    // condition. Experiment with its value can improve performance, since it
+    // is reducing number of context switches.
     bool init( Context & a_context, uint32_t a_spinLock = 100U);
     void deinit( Context & a_context);
 
+    // Calling enter/leave without calling init first will cause errors.
     void enter( Context & a_context);
     void leave( Context & a_context);
 }
@@ -109,20 +126,35 @@ namespace kernel::sync
     {
         ObjectSet,
         TimeoutOccurred,
-        WaitFailed,
+        WaitFailed,         // Return if any internal error occurred.
         InvalidHandle
     };
 
-    // Can wait for:
-    // Event, timer
-    // NOTE: destorying items used by this function will result in undefined behaviour
-    // todo: consider add task to possible signal types
+    // Can wait for system objects of type: Event, Timer.
+    // For event it is SET state, for Timer it is FINISHED state.
+    // If used with invalid handle, error is returned.
+    //
+    // The moment function is called, and prowivded signals are not set
+    // task will stop execution and enter Waiting state until conditions
+    // are not met.
+    //
+    // If a_wait_forver is true, then a_timeout is not used.
+    //
+    // Function return ObjectSet if signal condittion is met.
+    //
+    // NOTE: Destroying system objects used by this function will result
+    //       in undefined behaviour
     WaitResult waitForSingleObject(
         kernel::Handle &    a_handle,
         bool                a_wait_forver = true,
         Time_ms             a_timeout = 0U
     );
 
+    // Wait for multiple system objects provided as an array.
+    //
+    // If a_wait_for_all is set, task will wait until ALL signals are set.
+    // If a_wait_for_all is not set, optional argument a_signaled_item_index,
+    // will return first signaled handle index.
     WaitResult waitForMultipleObjects(
         kernel::Handle *    a_array_of_handles,
         uint32_t            a_number_of_elements,
